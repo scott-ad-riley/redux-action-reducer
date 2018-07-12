419d1dee09a4f9b3b40d9f8b67e851003feecf6b
Scott Riley <scott.riley111@gmail.com>
2018-07-12 23:17:19 +0100

    fixing indentation

--- modules/index.js
+++ modules/index.js
@@ -1,10 +1,10 @@
 const payloadPassThrough = (state, payload) => payload;
 
 const createReducer = (defaultValue = null, ...boundReducerCreators) => {
-  const boundReducers = boundReducerCreators.map(creator => creator(defaultValue))
-  return (state, payload) => boundReducers.reduce((acc, boundReducer) => {
-      return boundReducer(acc, payload);
-  }, state)
+    const boundReducers = boundReducerCreators.map(creator => creator(defaultValue))
+    return (state, payload) => boundReducers.reduce((acc, boundReducer) => {
+        return boundReducer(acc, payload);
+    }, state)
 }
 
 
@@ -17,25 +17,25 @@ export const whenSuccess = (reducer = payloadPassThrough) => (state, payload, er
     error ? state : reducer(state, payload);
 
 export const extendReducer = (reducer) => (defaultValue = null, ...boundReducers) => {
-  const extraReducer = createReducer(defaultValue, ...boundReducers);
-  return (state, action) =>
-    extraReducer(reducer(state, action), action);
+    const extraReducer = createReducer(defaultValue, ...boundReducers);
+    return (state, action) =>
+        extraReducer(reducer(state, action), action);
 }
 
 export const bindReducer = (actionOrActions, unboundReducer = payloadPassThrough) => {
-  const boundActions = [].concat(actionOrActions).map((actionType) => {
-    if (typeof actionType !== 'string') {
-      throw new Error('Action type must be a string, received: ' + actionType);
-    }
-    return [actionType, unboundReducer]
-  })
+    const boundActions = [].concat(actionOrActions).map((actionType) => {
+        if (typeof actionType !== 'string') {
+          throw new Error('Action type must be a string, received: ' + actionType);
+        }
+        return [actionType, unboundReducer]
+    })
 
-  return (defaultValue) => (state, { type, payload, error }) =>
-      boundActions.reduce((accState, [actionType, reducer]) => {
-          if (type === actionType) {
-              return reducer(accState, payload, error);
-          }
+    return (defaultValue) => (state, { type, payload, error }) =>
+        boundActions.reduce((accState, [actionType, reducer]) => {
+            if (type === actionType) {
+                return reducer(accState, payload, error);
+            }
 
-          return accState;
-      }, state)
+            return accState;
+        }, state)
 }
